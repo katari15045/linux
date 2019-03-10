1b82aa0ad4236979a083c67860e54ee46a92a70c
Saketh Katari <katari15045@iiitd.ac.in>
2019-03-10 22:48:29 +0530

    assembly: adds 3_print_digits

---
+++ assembly/3_print_digits/main.asm
@@ -0,0 +1,26 @@
+section .data
+	digit db 0, 10 ;10 represents new line
+
+section .text
+	global _start
+
+_start:
+	mov rax, 3
+	call print_digit
+	call wrap_up
+
+print_digit:
+	add rax, 48
+	mov [digit], al ; 8 least significant bits of rax are moved to the address stored in digit
+	mov rax, 1 ;sys_write
+	mov rdi, 1 ;std_out
+	mov rsi, digit
+	mov rdx, 2 ;no.of bytes of digit
+	syscall
+	ret
+
+wrap_up:
+	mov rax, 60 ;sys_exit
+	mov rdi, 0 ;exit code 0
+	syscall
+	ret
---
+++ assembly/README.txt
@@ -0,0 +1,1 @@
+Tutorials: https://docs.google.com/document/d/1hrPjIp0bhBNK1Gxi63nLIn14wkpwDkrBsc3HshglXuQ/edit?usp=sharing
